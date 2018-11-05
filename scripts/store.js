37a5311f8ed54a63296d1aa853e0cba792e84a8b
Stephen Anderson <anderssj@usc.edu>
2018-11-05 11:52:11 -0800

    adds find by ID

--- scripts/store.js
+++ scripts/store.js
@@ -1,14 +1,18 @@
 /*eslint-env jquery*/
-'use strict';
+'use strict'
 
 const store = (function() {
-  const foo = 'bar';
-  const items = [];
-  let hideCheckedItems = false;
+  const foo = 'bar'
+  const items = []
+  let hideCheckedItems = false
 
-  const addItem = function(itemName) {};
-  const updateItem = function(id, updateData) {};
-  const toggleHideCheckedFilter = function() {};
+  const addItem = function(itemName) {}
+  const updateItem = function(id, updateData) {}
+  const toggleHideCheckedFilter = function() {}
+
+  const findById = function(id) {
+    return items.find(id)
+  }
 
   return {
     items,
@@ -16,5 +20,5 @@ const store = (function() {
     addItem,
     updateItem,
     toggleHideCheckedFilter
-  };
-})();
+  }
+})()
