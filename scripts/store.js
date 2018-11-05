c1243e6e658c6cb48962d10c34fac92073b75853
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-05 14:53:06 -0700

    completed step 4

--- scripts/store.js
+++ scripts/store.js
@@ -1,38 +1,43 @@
 /*eslint-env jquery*/
-'use strict'
+'use strict';
 
 const store = (function() {
-  const items = []
-  let hideCheckedItems = false
+  const items = [];
+  let hideCheckedItems = false;
 
   const addItem = function(name) {
     try {
-      this.items.httvalidateName(name)
-      this.items.push(name.create(name))
+      Item.validateName(name);
+      this.items.push(Item.create(name));
     } catch (error) {
-      console.log('cant add item ' + error.message)
+      console.log('cant add item ' + error.message);
     }
-  }
+  };
   const findAndToggleChecked = function(id) {
     this.findById(element => {
-      element.checked = !element.checked
-    })
-  }
-  const updateItem = function(id, updateData) {}
-  const toggleHideCheckedFilter = function() {}
+      element.checked = !element.checked;
+    });
+  };
+  const updateItem = function(id, updateData) {};
+  const toggleHideCheckedFilter = function() {};
 
   const findById = function(id) {
-    return this.items.find(element => element.id === id)
-  }
+    return this.items.find(element => element.id === id);
+  };
 
   const findAndUpdateName = function(id, newName) {
     try {
-      Item.validateName(newName)
-      this.findById(id).name = newName
+      this.Item.validateName(newName);
+      this.findById(id).name = newName;
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-  }
+  };
+
+  const findAndDelete = function(id) {
+    return this.items.filter(item => item.id !== id);
+    // console.log('find and delete running!');
+  };
 
   return {
     items,
@@ -42,6 +47,7 @@ const store = (function() {
     toggleHideCheckedFilter,
     findAndUpdateName,
     findById,
-    findAndToggleChecked
-  }
-})()
+    findAndToggleChecked,
+    findAndDelete
+  };
+})();
