f5b7c42100579069011a50f044b9f7f6babd0421
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-05 10:45:15 -0700

    Item factory and validation

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,16 +1,27 @@
 /*eslint-env jquery*/
+/* global cuid */
 'use strict';
 
 const Item = (function() {
-//   const foo = 'bar';
-//   const items = [];
-//   let hideCheckedItems = false;
+  const foo = 'bar';
+  //   const items = [];
+  //   let hideCheckedItems = false;
 
-//   const addItem = function(itemName) {};
-//   const updateItem = function(id, updateData) {};
-//   const toggleHideCheckedFilter = function() {};
-
+  //   const addItem = function(itemName) {};
+  //   const updateItem = function(id, updateData) {};
+  //   const toggleHideCheckedFilter = function() {};
+  function validateName(name) {
+    if (!name) throw new Error('Name does not exist');
+  }
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false
+    };
+  };
   return {
-
+    validateName,
+    create
   };
 })();
--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,22 @@
 /*eslint-env jquery*/
 'use strict';
 
-/* global shoppingList, cuid */
+/* global shoppingList, Item, store */
 
 // eslint-disable-next-line no-unused-vars
 
 $(document).ready(function() {
+  const itemNames = ['', 'apples', 'pears'];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch (error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
+  shoppingList.render();
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
 
-console.log(Item);
-console.log(foo);
