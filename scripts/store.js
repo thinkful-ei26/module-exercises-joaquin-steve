3c63b0d6852863d26265913311d132c71b502b20
Joaquin Fox <joaquin.fox@gmail.com>
2018-11-05 13:10:13 -0700

    added methods to store.js

--- scripts/store.js
+++ scripts/store.js
@@ -6,9 +6,25 @@ const store = (function() {
   const items = [];
   let hideCheckedItems = false;
 
-  const addItem = function(itemName) {};
+  const addItem = function(name) {
+    try {
+      name.validateName(name);
+      this.items.push(name.create(name));
+    } catch (error) {
+      console.log('cant add item ' + error.message);
+    }
+  };
+  const findAndToggleChecked = function(id) {
+    this.findById(element => {
+      element.checked = !element.checked;
+    });
+  };
   const updateItem = function(id, updateData) {};
   const toggleHideCheckedFilter = function() {};
+
+  function findBy(id) {
+    id.find(element => element.id);
+  }
 
   return {
     items,
