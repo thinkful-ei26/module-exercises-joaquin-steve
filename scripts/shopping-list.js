44c4fd85c3d21944f0352b71c0b77d24ade024a7
Stephen Anderson <anderssj@usc.edu>
2018-11-05 12:51:42 -0800

    findAndDelete is working

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,5 +1,5 @@
 /*eslint-env jquery*/
-'use strict';
+'use strict'
 
 /* global store, cuid */
 
@@ -8,13 +8,13 @@ const shoppingList = (function() {
   function generateItemElement(item) {
     let itemTitle = `<span class="shopping-item shopping-item__checked">${
       item.name
-    }</span>`;
+    }</span>`
     if (!item.checked) {
       itemTitle = `
         <form class="js-edit-item">
           <input class="shopping-item type="text" value="${item.name}" />
         </form>
-      `;
+      `
     }
 
     return `
@@ -28,142 +28,141 @@ const shoppingList = (function() {
             <span class="button-label">delete</span>
           </button>
         </div>
-      </li>`;
+      </li>`
   }
 
   function generateShoppingItemsString(shoppingList) {
-    const items = shoppingList.map(item => generateItemElement(item));
-    return items.join('');
+    const items = shoppingList.map(item => generateItemElement(item))
+    return items.join('')
   }
 
   function render() {
     // Filter item list if store prop is true by item.checked === false
-    let items = store.items;
+    let items = store.items
     if (store.hideCheckedItems) {
-      items = store.items.filter(item => !item.checked);
+      items = store.items.filter(item => !item.checked)
     }
 
     // Filter item list if store prop `searchTerm` is not empty
     if (store.searchTerm) {
-      items = store.items.filter(item => item.name.includes(store.searchTerm));
+      items = store.items.filter(item => item.name.includes(store.searchTerm))
     }
 
     // render the shopping list in the DOM
-    console.log('`render` ran');
-    const shoppingListItemsString = generateShoppingItemsString(items);
+    console.log('`render` ran')
+    const shoppingListItemsString = generateShoppingItemsString(items)
 
     // insert that HTML into the DOM
-    $('.js-shopping-list').html(shoppingListItemsString);
+    $('.js-shopping-list').html(shoppingListItemsString)
   }
 
   function addItemToShoppingList(itemName) {
     try {
-      Item.validateName(itemName);
-      store.items.push(Item.create(itemName));
+      Item.validateName(itemName)
+      store.items.push(Item.create(itemName))
     } catch (error) {
-      console.log('cant add item ' + error.message);
+      console.log('cant add item ' + error.message)
     }
-    shoppingList.render();
+    shoppingList.render()
   }
 
   function handleNewItemSubmit() {
     $('#js-shopping-list-form').submit(function(event) {
-      event.preventDefault();
-      const newItemName = $('.js-shopping-list-entry').val();
-      $('.js-shopping-list-entry').val('');
-      addItemToShoppingList(newItemName);
-      render();
-    });
+      event.preventDefault()
+      const newItemName = $('.js-shopping-list-entry').val()
+      $('.js-shopping-list-entry').val('')
+      addItemToShoppingList(newItemName)
+      render()
+    })
   }
 
   function toggleCheckedForListItem(id) {
-    const foundItem = store.items.find(item => item.id === id);
-    foundItem.checked = !foundItem.checked;
+    const foundItem = store.items.find(item => item.id === id)
+    foundItem.checked = !foundItem.checked
   }
 
   function getItemIdFromElement(item) {
     return $(item)
       .closest('.js-item-element')
-      .data('item-id');
+      .data('item-id')
   }
 
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
-      const id = getItemIdFromElement(event.currentTarget);
-      toggleCheckedForListItem(id);
-      render();
-    });
+      const id = getItemIdFromElement(event.currentTarget)
+      toggleCheckedForListItem(id)
+      render()
+    })
   }
 
   function deleteListItem(id) {
-    const index = store.items.findIndex(item => item.id === id);
-    store.items.splice(index, 1);
+    const index = store.items.findIndex(item => item.id === id)
+    store.items.splice(index, 1)
   }
 
   function editListItemName(id, itemName) {
-    const item = store.items.find(item => item.id === id);
-    item.name = itemName;
+    store.findAndUpdateName(id, itemName)
   }
 
   function toggleCheckedItemsFilter() {
-    store.hideCheckedItems = !store.hideCheckedItems;
+    store.hideCheckedItems = !store.hideCheckedItems
   }
 
   function setSearchTerm(val) {
-    store.searchTerm = val;
+    store.searchTerm = val
   }
 
   function handleDeleteItemClicked() {
     // like in `handleItemCheckClicked`, we use event delegation
     $('.js-shopping-list').on('click', '.js-item-delete', event => {
       // get the index of the item in store.items
-      const id = getItemIdFromElement(event.currentTarget);
+      const id = getItemIdFromElement(event.currentTarget)
       // delete the item
-      deleteListItem(id);
+      deleteListItem(id)
       // render the updated shopping list
-      render();
-    });
+      render()
+    })
   }
 
   function handleEditShoppingItemSubmit() {
     $('.js-shopping-list').on('submit', '.js-edit-item', event => {
-      event.preventDefault();
-      const id = getItemIdFromElement(event.currentTarget);
+      event.preventDefault()
+      const id = getItemIdFromElement(event.currentTarget)
       const itemName = $(event.currentTarget)
         .find('.shopping-item')
-        .val();
-      editListItemName(id, itemName);
-      render();
-    });
+        .val()
+      editListItemName(id, itemName)
+      render()
+    })
   }
 
   function handleToggleFilterClick() {
     $('.js-filter-checked').click(() => {
-      toggleCheckedItemsFilter();
-      render();
-    });
+      toggleCheckedItemsFilter()
+      render()
+    })
   }
 
   function handleShoppingListSearch() {
     $('.js-shopping-list-search-entry').on('keyup', event => {
-      const val = $(event.currentTarget).val();
-      setSearchTerm(val);
-      render();
-    });
+      const val = $(event.currentTarget).val()
+      setSearchTerm(val)
+      render()
+    })
   }
 
   function bindEventListeners() {
-    handleNewItemSubmit();
-    handleItemCheckClicked();
-    handleDeleteItemClicked();
-    handleEditShoppingItemSubmit();
-    handleToggleFilterClick();
-    handleShoppingListSearch();
+    handleNewItemSubmit()
+    handleItemCheckClicked()
+    handleDeleteItemClicked()
+    handleEditShoppingItemSubmit()
+    handleToggleFilterClick()
+    handleShoppingListSearch()
   }
 
   // This object contains the only exposed methods from this module:
   return {
     render: render,
     bindEventListeners: bindEventListeners
-  };
-})();
+  }
+})()
--- scripts/store.js
+++ scripts/store.js
@@ -1,36 +1,47 @@
 /*eslint-env jquery*/
-'use strict';
+'use strict'
 
 const store = (function() {
-  const foo = 'bar';
-  const items = [];
-  let hideCheckedItems = false;
+  const foo = 'bar'
+  const items = []
+  let hideCheckedItems = false
 
   const addItem = function(name) {
     try {
-      name.validateName(name);
-      this.items.push(name.create(name));
+      this.items.httvalidateName(name)
+      this.items.push(name.create(name))
     } catch (error) {
-      console.log('cant add item ' + error.message);
+      console.log('cant add item ' + error.message)
     }
-  };
+  }
   const findAndToggleChecked = function(id) {
     this.findById(element => {
-      element.checked = !element.checked;
-    });
-  };
-  const updateItem = function(id, updateData) {};
-  const toggleHideCheckedFilter = function() {};
+      element.checked = !element.checked
+    })
+  }
+  const updateItem = function(id, updateData) {}
+  const toggleHideCheckedFilter = function() {}
 
   const findById = function(id) {
-    return items.find(id);
-  };
+    return this.items.find(element => element.id === id)
+  }
+
+  const findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(newName)
+      this.findById(id).name = newName
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   return {
     items,
     hideCheckedItems,
     addItem,
     updateItem,
-    toggleHideCheckedFilter
-  };
-})();
+    toggleHideCheckedFilter,
+    findAndUpdateName,
+    findById
+  }
+})()
